17f6e81589e6dd66a20df19bc8e6c7c75ae68bec
413d <20566780+413d@users.noreply.github.com>
2025-03-02 22:15:36 +0300

    feat(app): add error boundary

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,4 +1,5 @@
 import { appConfig } from '@/shared/config';
+import { Alert, AlertDescription, AlertTitle, Button, ErrorBoundary } from '@/shared/ui';
 import { ConvertPage } from '@/pages/convert';
 import { ThemeProvider, ThemeToggle } from '@/features/theming';
 import './app.css';
@@ -7,10 +8,29 @@ export const App = () => (
   <ThemeProvider defaultTheme="dark">
     <header className="p-2 border-b border-secondary flex justify-between items-center">
       <h1 className="text-xl font-mono">{ appConfig.title }</h1>
-      <ThemeToggle />
+      <ErrorBoundary fallback={' '}>
+        <ThemeToggle />
+      </ErrorBoundary>
     </header>
     <main className="p-2">
-      <ConvertPage />
+      <ErrorBoundary
+        fallback={appConfig.isProd ? (
+          <Alert variant="default" className="space-y-2">
+            <AlertTitle>Failed to load currency converter</AlertTitle>
+            <AlertDescription>
+              <Button
+                className="cursor-pointer"
+                size="lg"
+                onClick={() => window.location.reload()}
+              >
+                Retry
+              </Button>
+            </AlertDescription>
+          </Alert>
+        ) : undefined}
+      >
+        <ConvertPage />
+      </ErrorBoundary>
     </main>
   </ThemeProvider>
 );
