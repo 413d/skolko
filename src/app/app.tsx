7e4e65d16bf3a73a8068239b903b031f7962cbf3
413d <20566780+413d@users.noreply.github.com>
2025-03-10 19:53:11 +0300

    fix(app): minor root layout improvements

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,39 +1,34 @@
-import { appConfig } from '@/shared/config';
-import { Alert, AlertDescription, AlertTitle, Button, ErrorBoundary } from '@/shared/ui';
-
-import { ConvertPage } from '@/pages/convert';
-import { ThemeProvider, ThemeToggle } from '@/features/theming';
-
 import './app.css';
+
+import { appConfig } from '@/shared/config';
+import { Button, ErrorBoundary } from '@/shared/ui';
+
+import { ThemeProvider, ThemeToggle } from '@/features/theming';
+import { ConvertPage } from '@/pages/convert';
 
 export const App = () => (
   <ThemeProvider defaultTheme="dark">
-    <header className="p-2 border-b border-secondary flex justify-between items-center">
-      <a href="/">
+    <header className="p-2 flex justify-between items-center border-b border-secondary">
+      <a href="/" title={appConfig.title}>
         <img className="w-10 dark:invert" src="/skolko.svg" alt={appConfig.title} />
       </a>
-      <h1 className="text-xl font-mono">{ appConfig.title }</h1>
+
+      <h1 className="text-2xl font-mono tracking-wider">{ appConfig.title }</h1>
+
       <ErrorBoundary fallback={' '}>
         <ThemeToggle />
       </ErrorBoundary>
     </header>
     <main className="p-2">
-      <ErrorBoundary
-        fallback={appConfig.isProd ? (
-          <Alert variant="default" className="space-y-2">
-            <AlertTitle>Failed to load currency converter</AlertTitle>
-            <AlertDescription>
-              <Button
-                className="cursor-pointer"
-                size="lg"
-                onClick={() => window.location.reload()}
-              >
-                Retry
-              </Button>
-            </AlertDescription>
-          </Alert>
-        ) : undefined}
-      >
+      <ErrorBoundary description={appConfig.isProd ? (
+        <Button
+          className="cursor-pointer"
+          size="lg"
+          onClick={() => typeof window !== 'undefined' && window.location.reload()}
+        >
+          Retry
+        </Button>
+      ) : undefined}>
         <ConvertPage />
       </ErrorBoundary>
     </main>
