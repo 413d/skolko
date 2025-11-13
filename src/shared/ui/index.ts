804c37915303233c1ddb0dee589f1693b8362640
413d <20566780+413d@users.noreply.github.com>
2025-11-13 15:36:47 +0300

    feat(ui): toasts component

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -7,3 +7,4 @@ export * from './components/command';
 export * from './components/dialog';
 export * from './components/input';
 export * from './components/popover';
+export * from './components/sonner';
