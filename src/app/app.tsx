5b9592ddd039a07468b2bcfc70caeb40e5f911ef
413d <20566780+413d@users.noreply.github.com>
2025-12-11 18:00:52 +0100

    fix(app): nano optimizations

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,20 +1,23 @@
 import './app.css';
 
+import { lazy, Suspense } from 'react';
+
 import { appConfig } from '@/shared/config';
 import { Button, ErrorBoundary, Toaster } from '@/shared/ui';
 
 import { ThemeToggle, useTheme } from '@/features/theming';
-import { ConvertPage } from '@/pages/convert';
 
 import { Providers } from './providers';
 
 import logo from '/skolko.svg';
 
-export const App = () => {
+const ConvertPage = lazy(() => import('@/pages/convert/ui/convert-page').then(m => ({ default: m.ConvertPage })));
+
+const AppContent = () => {
   const { theme } = useTheme();
 
   return (
-    <Providers>
+    <>
       <header className="p-2 flex justify-between items-center border-b border-secondary">
         <a href={appConfig.baseUrl} title={appConfig.title}>
           <img className="w-10 dark:invert" src={logo} alt={appConfig.title} />
@@ -36,10 +39,18 @@ export const App = () => {
           Retry
           </Button>
         ) : undefined}>
-          <ConvertPage />
+          <Suspense>
+            <ConvertPage />
+          </Suspense>
         </ErrorBoundary>
       </main>
       <Toaster theme={theme} />
-    </Providers>
+    </>
   );
 };
+
+export const App = () => (
+  <Providers>
+    <AppContent />
+  </Providers>
+);
